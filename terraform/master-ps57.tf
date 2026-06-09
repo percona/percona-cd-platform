04fb4e5fe4ae5873dad4d4a9f1d527052e5958e0
Ando Nogueira <anderson.nogueira@percona.com>
2026-06-10 01:06:48 +0200

    feat(arm-fleets): widen spot pools to 8 types, price-capacity-optimized (#132)
    - 6 pools (3 types x 2 AZs) let capacity-optimized concentrate the whole
      ps80 fleet into m7g.2xlarge us-west-2b; one pool squeeze killed three
      busy workers in 25 minutes on 2026-06-08
    - Widen every fleet to m8g/m7g/m6g/m7gd/m6gd/r8g/r7g/r6g 2xlarge (all
      verified offered in all five fleet regions; m8gd excluded, missing in
      us-west-1) for 16 pools per fleet
    - Switch both fleet modules to price-capacity-optimized, the
      AWS-recommended strategy balancing pool depth and price

--- terraform/master-ps57.tf
+++ terraform/master-ps57.tf
@@ -113,7 +113,7 @@ module "ps57_arm_fleet" {
   worker_instance_profile_name = module.ps57.worker_instance_profile_name
   master_role_name             = module.ps57.master_iam_role_name
   key_name                     = "percona-jenkins"
-  instance_types               = ["m8g.2xlarge", "m7g.2xlarge", "m6g.2xlarge"]
+  instance_types               = ["m8g.2xlarge", "m7g.2xlarge", "m6g.2xlarge", "m7gd.2xlarge", "m6gd.2xlarge", "r8g.2xlarge", "r7g.2xlarge", "r6g.2xlarge"]
   max_size                     = 16
   tickets                      = "PS-11179"
 }
