41f15669d13c6166b73fa6d664321e4c5e30f3e3
Anderson Nogueira <anderson.nogueira@percona.com>
2026-06-10 05:02:08 +0000

    feat(cloud): migrate master CFN to TF, EKS-fronted with Graviton fleet repoint
    - master-cloud.tf: jenkins-master module (fresh VPC at 10.177.0.0/22, on-demand c7i-flex.xlarge, cloud-build-cache), one-shot import of the retained 100 GiB gp2 data volume, EKS hub peering + routes
    - Repoint cloud_arm_fleet from data-source lookups to module outputs
    - Codify the live master's five init.groovy.d files under resources/jenkins-masters/cloud/, retiring the classic docker-32gb-aarch64 template (the Graviton fleet is the label's sole provider)
    - Add cloud to jenkins-endpoint-reconciler and jenkins-ingress hosts
    - variables.tf: cloud proxy entry flips to reconciler-discovered upstream

---
+++ resources/jenkins-masters/cloud/init.groovy.d/htz.cloud.groovy
@@ -0,0 +1,184 @@
+import cloud.dnation.jenkins.plugins.hetzner.*
+import cloud.dnation.jenkins.plugins.hetzner.launcher.*
+import hudson.model.*
+import jenkins.model.Jenkins
+import java.util.logging.Logger
+
+def cloudName = "cloud-htz"
+
+imageMap = [:]                          // ID          TYPE     NAME                 DESCRIPTION          ARCHITECTURE   IMAGE SIZE   DISK SIZE   CREATED                         DEPRECATED
+imageMap['fedora42-x64']     = '342035601' // 342035601   system   fedora-43            Fedora 43            x86
+imageMap['fedora42-aarch64'] = '342035605' // 342035605   system   fedora-43            Fedora 43            arm
+imageMap['launcher-x64']  = imageMap['fedora42-x64']
+
+execMap = [:]
+execMap['fedora']                = 1
+execMap['fedora42-x64-nbg1']     = execMap['fedora']
+execMap['fedora42-x64-hel1']     = execMap['fedora']
+execMap['fedora42-x64-fsn1']     = execMap['fedora']
+execMap['fedora42-aarch64-nbg1'] = execMap['fedora']
+execMap['fedora42-aarch64-hel1'] = execMap['fedora']
+execMap['fedora42-aarch64-fsn1'] = execMap['fedora']
+execMap['fedora42-x64-nbg1-min']     = execMap['fedora']
+execMap['fedora42-x64-hel1-min']     = execMap['fedora']
+execMap['fedora42-x64-fsn1-min']     = execMap['fedora']
+execMap['fedora42-aarch64-nbg1-min'] = execMap['fedora']
+execMap['fedora42-aarch64-hel1-min'] = execMap['fedora']
+execMap['fedora42-aarch64-fsn1-min'] = execMap['fedora']
+execMap['launcher-x64-nbg1']  = 30
+execMap['launcher-x64-hel1']  = 30
+execMap['launcher-x64-fsn1']  = 30
+
+bootDeadlineMap =[:]
+bootDeadlineMap['default']            = 7
+bootDeadlineMap['fedora42-x64-nbg1']     = bootDeadlineMap['default']
+bootDeadlineMap['fedora42-x64-hel1']     = bootDeadlineMap['default']
+bootDeadlineMap['fedora42-x64-fsn1']     = bootDeadlineMap['default']
+bootDeadlineMap['fedora42-aarch64-nbg1'] = bootDeadlineMap['default']
+bootDeadlineMap['fedora42-aarch64-hel1'] = bootDeadlineMap['default']
+bootDeadlineMap['fedora42-aarch64-fsn1'] = bootDeadlineMap['default']
+bootDeadlineMap['fedora42-x64-nbg1-min']     = bootDeadlineMap['default']
+bootDeadlineMap['fedora42-x64-hel1-min']     = bootDeadlineMap['default']
+bootDeadlineMap['fedora42-x64-fsn1-min']     = bootDeadlineMap['default']
+bootDeadlineMap['fedora42-aarch64-nbg1-min'] = bootDeadlineMap['default']
+bootDeadlineMap['fedora42-aarch64-hel1-min'] = bootDeadlineMap['default']
+bootDeadlineMap['fedora42-aarch64-fsn1-min'] = bootDeadlineMap['default']
+bootDeadlineMap['launcher-x64-nbg1']  = bootDeadlineMap['default']
+bootDeadlineMap['launcher-x64-hel1']  = bootDeadlineMap['default']
+bootDeadlineMap['launcher-x64-fsn1']  = bootDeadlineMap['default']
+
+jvmOptsMap = [:]
+jvmOptsMap['fedora42']         = '-Xms4g -Xmx16g -Xss4m -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED'
+jvmOptsMap['fedora42-x64-nbg1']     = jvmOptsMap['fedora42']
+jvmOptsMap['fedora42-x64-hel1']     = jvmOptsMap['fedora42']
+jvmOptsMap['fedora42-x64-fsn1']     = jvmOptsMap['fedora42']
+jvmOptsMap['fedora42-aarch64-nbg1'] = jvmOptsMap['fedora42']
+jvmOptsMap['fedora42-aarch64-hel1'] = jvmOptsMap['fedora42']
+jvmOptsMap['fedora42-aarch64-fsn1'] = jvmOptsMap['fedora42']
+jvmOptsMap['fedora42-x64-nbg1-min']     = jvmOptsMap['fedora42']
+jvmOptsMap['fedora42-x64-hel1-min']     = jvmOptsMap['fedora42']
+jvmOptsMap['fedora42-x64-fsn1-min']     = jvmOptsMap['fedora42']
+jvmOptsMap['fedora42-aarch64-nbg1-min'] = jvmOptsMap['fedora42']
+jvmOptsMap['fedora42-aarch64-hel1-min'] = jvmOptsMap['fedora42']
+jvmOptsMap['fedora42-aarch64-fsn1-min'] = jvmOptsMap['fedora42']
+jvmOptsMap['launcher-x64-nbg1']  = jvmOptsMap['fedora42']
+jvmOptsMap['launcher-x64-hel1']  = jvmOptsMap['fedora42']
+jvmOptsMap['launcher-x64-fsn1']  = jvmOptsMap['fedora42']
+
+labelMap = [:]
+labelMap['fedora42-x64-min']     = 'docker-x64-min docker-fedora42-x64-min fedora42-x64-min'
+labelMap['fedora42-aarch64-min'] = 'docker-aarch64-min docker-fedora42-aarch64-min fedora42-aarch64-min'
+labelMap['fedora42-x64']         = 'docker-x64 docker-fedora42-x64 fedora42-x64'
+labelMap['fedora42-aarch64']     = 'docker-aarch64 docker-fedora42-aarch64 fedora42-aarch64'
+labelMap['launcher-x64']      = 'launcher-x64'
+
+networkMap = [:]
+networkMap['cloud.cd.percona.com'] = '11334955' // cloud.cd.percona.com
+
+initMap = [:]
+initMap['fedora-docker'] = '''#!/bin/bash -x
+    set -o xtrace
+    echo -e "nameserver 9.9.9.9\nnameserver 1.1.1.1" | sudo tee /etc/resolv.conf
+    echo '10.30.6.9 repo.ci.percona.com' | sudo tee -a /etc/hosts
+    sudo install -o $(id -u -n) -g $(id -g -n) -d /mnt/jenkins
+    sudo fallocate -l 32G /swapfile
+    sudo chmod 600 /swapfile
+    sudo mkswap /swapfile
+    sudo swapon /swapfile
+    until sudo dnf update -y; do
+        sleep 1
+        echo "try again"
+    done
+    until sudo dnf install -y java-21-openjdk-headless ca-certificates curl gnupg unzip git dnf-plugins-core cronie bc npm make qemu-user-static; do
+        sleep 1
+        echo "try again"
+    done
+    sudo dnf config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
+    until sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin; do
+        sleep 1
+        echo "try again"
+    done
+    if ! $(aws --version | grep -q 'aws-cli/2'); then
+        find /tmp -maxdepth 1 -name "*aws*" | xargs sudo rm -rf
+        until curl "https://awscli.amazonaws.com/awscli-exe-linux-$(uname -m).zip" -o "/tmp/awscliv2.zip"; do
+            sleep 1
+            echo try again
+        done
+        unzip -o /tmp/awscliv2.zip -d /tmp
+        cd /tmp/aws && sudo ./install
+    fi
+    sudo sysctl net.ipv4.tcp_fin_timeout=15
+    sudo sysctl net.ipv4.tcp_tw_reuse=1
+    sudo sysctl net.ipv6.conf.all.disable_ipv6=1
+    sudo sysctl net.ipv6.conf.default.disable_ipv6=1
+    sudo sysctl -w fs.inotify.max_user_watches=10000000 || true
+    sudo sysctl -w fs.aio-max-nr=1048576 || true
+    sudo sysctl -w fs.file-max=6815744 || true
+    echo "*  soft  core  unlimited" | sudo tee -a /etc/security/limits.conf
+    sudo sed -i.bak -e 's/nofile=1024:4096/nofile=900000:900000/; s/DAEMON_MAXFILES=.*/DAEMON_MAXFILES=990000/' /etc/sysconfig/docker
+    echo 'DOCKER_STORAGE_OPTIONS="--data-root=/mnt/docker"' | sudo tee -a /etc/sysconfig/docker-storage
+    sudo sed -i.bak -e 's|^ExecStart=.*|ExecStart=/usr/bin/dockerd --data-root=/mnt/docker --default-ulimit nofile=900000:900000|' /usr/lib/systemd/system/docker.service
+    sudo systemctl daemon-reload
+    sudo install -o root -g root -d /mnt/docker
+    sudo usermod -aG docker $(id -u -n)
+    sudo mkdir -p /etc/docker
+    echo '{"experimental": true, "ipv6": true, "fixed-cidr-v6": "fd3c:a8b0:18eb:5c06::/64"}' | sudo tee /etc/docker/daemon.json
+    sudo systemctl status docker || sudo systemctl start docker
+'''
+initMap['fedora42-x64-nbg1']     = initMap['fedora-docker']
+initMap['fedora42-x64-hel1']     = initMap['fedora-docker']
+initMap['fedora42-x64-fsn1']     = initMap['fedora-docker']
+initMap['fedora42-aarch64-nbg1'] = initMap['fedora-docker']
+initMap['fedora42-aarch64-hel1'] = initMap['fedora-docker']
+initMap['fedora42-aarch64-fsn1'] = initMap['fedora-docker']
+initMap['fedora42-x64-nbg1-min']     = initMap['fedora-docker']
+initMap['fedora42-x64-hel1-min']     = initMap['fedora-docker']
+initMap['fedora42-x64-fsn1-min']     = initMap['fedora-docker']
+initMap['fedora42-aarch64-nbg1-min'] = initMap['fedora-docker']
+initMap['fedora42-aarch64-hel1-min'] = initMap['fedora-docker']
+initMap['fedora42-aarch64-fsn1-min'] = initMap['fedora-docker']
+initMap['launcher-x64-nbg1']  = initMap['fedora-docker']
+initMap['launcher-x64-hel1']  = initMap['fedora-docker']
+initMap['launcher-x64-fsn1']  = initMap['fedora-docker']
+
+def templates = [
+       /* new HetznerServerTemplate("ubuntu20-cx21", "java", "name=ubuntu20-docker", "fsn1", "cx21"), */
+        //                        tmplName                  tmplLabels                     tmplImage                  region server type
+        new HetznerServerTemplate("fedora42-x64-nbg1-min",     labelMap['fedora42-x64-min'],     imageMap['fedora42-x64'],     "nbg1", "cpx42"),
+        new HetznerServerTemplate("fedora42-aarch64-nbg1-min", labelMap['fedora42-aarch64-min'], imageMap['fedora42-aarch64'], "nbg1", "cax31"),
+        new HetznerServerTemplate("fedora42-x64-hel1-min",     labelMap['fedora42-x64-min'],     imageMap['fedora42-x64'],     "hel1", "cpx42"),
+        new HetznerServerTemplate("fedora42-aarch64-hel1-min", labelMap['fedora42-aarch64-min'], imageMap['fedora42-aarch64'], "hel1", "cax31"),
+        new HetznerServerTemplate("fedora42-x64-fsn1-min",     labelMap['fedora42-x64-min'],     imageMap['fedora42-x64'],     "fsn1", "cpx42"),
+        new HetznerServerTemplate("fedora42-aarch64-fsn1-min", labelMap['fedora42-aarch64-min'], imageMap['fedora42-aarch64'], "fsn1", "cax31"),
+        new HetznerServerTemplate("fedora42-x64-nbg1",         labelMap['fedora42-x64'],         imageMap['fedora42-x64'],     "nbg1", "cpx62"),
+        new HetznerServerTemplate("fedora42-aarch64-nbg1",     labelMap['fedora42-aarch64'],     imageMap['fedora42-aarch64'], "nbg1", "cax41"),
+        new HetznerServerTemplate("fedora42-x64-hel1",         labelMap['fedora42-x64'],         imageMap['fedora42-x64'],     "hel1", "cpx62"),
+        new HetznerServerTemplate("fedora42-aarch64-hel1",     labelMap['fedora42-aarch64'],     imageMap['fedora42-aarch64'], "hel1", "cax41"),
+        new HetznerServerTemplate("fedora42-x64-fsn1",         labelMap['fedora42-x64'],         imageMap['fedora42-x64'],     "fsn1", "cpx62"),
+        new HetznerServerTemplate("fedora42-aarch64-fsn1",     labelMap['fedora42-aarch64'],     imageMap['fedora42-aarch64'], "fsn1", "cax41"),
+        new HetznerServerTemplate("launcher-x64-nbg1",      labelMap['launcher-x64'],      imageMap['launcher-x64'],  "nbg1", "cpx22"),
+        new HetznerServerTemplate("launcher-x64-hel1",      labelMap['launcher-x64'],      imageMap['launcher-x64'],  "hel1", "cpx22"),
+        new HetznerServerTemplate("launcher-x64-fsn1",      labelMap['launcher-x64'],      imageMap['launcher-x64'],  "fsn1", "cpx22")
+]
+
+templates.each { it ->
+                       def sshConnector = new SshConnectorAsRoot("htz.cd.key")
+                       sshConnector.setConnectionMethod(new PublicAddressOnly())  // Replace with the desired method
+                       it.setConnector(sshConnector)
+                       def tmplName = it.name
+                       it.setNumExecutors(execMap[tmplName])
+                       it.bootDeadline = bootDeadlineMap[tmplName]
+                       it.remoteFs = "/mnt/jenkins/"
+                       it.jvmOpts = jvmOptsMap[tmplName]
+                       it.network = networkMap['cloud.cd.percona.com']
+                       it.userData = initMap[tmplName]
+               }
+
+// public HetznerCloud(String name, String credentialsId, String instanceCapStr, List<HetznerServerTemplate> serverTemplates)
+def cloud = new HetznerCloud(cloudName, "htz.cd.token", "100", templates)
+
+def jenkins = Jenkins.get()
+
+jenkins.clouds.remove(jenkins.clouds.getByName(cloudName))
+jenkins.clouds.add(cloud)
+jenkins.save()
--- terraform/master-cloud.tf
+++ terraform/master-cloud.tf
@@ -1,37 +1,177 @@
 # Owner: cloud
-# ARM Graviton spot fleet for cloud, Fleet-only (still-CloudFormation master).
-# Provisions ONLY the worker ASG in Terraform; the cloud master stays CFN-managed in
-# eu-west-1, so no CFN-to-TF cutover is required. The ec2-fleet plugin (installed
-# on cloud in a separately announced idle window) drives the ASG via the CFN-managed
-# jenkins-cloud-master role; the jenkins-arm-fleet module attaches the autoscaling IAM
-# policy by role NAME, so no Terraform management of that role is required.
-# Capacity-optimized across m8g/m7g/m6g.2xlarge. Folds into the later CFN->TF migration
-# by swapping the data sources for module.cloud.* outputs.
-data "aws_vpc" "cloud" {
-  provider = aws.eu-west-1
-  tags     = { Name = "jenkins-cloud" }
-}
+# cloud.cd.percona.com -- Terraform-managed via ./modules/jenkins-master/.
+# Migrated from CloudFormation stack jenkins-cloud
+# (Percona-Lab/jenkins-pipelines/IaC/cloud.cd).
+#
+# Same cutover as ps3/ps80/ps57/pxb/pxc/psmdb: off the AWS SpotFleet onto a
+# single on-demand instance (purchasing_option), EKS-fronted (TLS terminates
+# at the jenkins-masters ALB, reached over cross-region VPC peering on
+# :8080), declarative init.groovy.d via the module S3 bucket, and the
+# Graviton ARM EC2 Fleet fallback (pre-provisioned while cloud was still CFN;
+# repointed below to the module VPC outputs).
+#
+# cloud-specific deltas: fresh VPC reusing the same 10.177.0.0/22 (cloud kept
+# this CIDR as the sole occupant after the pxc/ps57 re-CIDRs, so it is
+# fleet-unique and can peer); the codified cloud.groovy retires the classic
+# docker-32gb-aarch64 template so the Graviton fleet is the label's sole
+# provider. The retained 100 GiB gp2 data volume is imported in eu-west-1b
+# (az_index 1, the module default; EBS is AZ-bound so the on-demand instance
+# lands there too).
+module "cloud" {
+  source    = "./modules/jenkins-master"
+  providers = { aws = aws.eu-west-1 }
 
-data "aws_subnets" "cloud" {
-  provider = aws.eu-west-1
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.cloud.id]
+  hostname                = "cloud.cd.percona.com"
+  short_name              = "jenkins-cloud"
+  team                    = "cloud"
+  vpc_cidr                = "10.177.0.0/22"
+  ami_id                  = nonsensitive(data.aws_ssm_parameter.al2023_minimal_euw1.value) # latest AL2023 minimal (amis.tf)
+  master_profile          = "eks_observability"
+  jenkins_package_version = "2.541.3"
+  cache_bucket_name       = "cloud-build-cache"
+
+  # Retained CFN data volume vol-077265221bc0180b3 is 100 GiB gp2 in
+  # eu-west-1b. ebs_type must be gp2 (not the module default gp3) so the
+  # tofu import is a zero-diff adopt; encrypted/iops are ignore_changes in
+  # the module.
+  ebs_size = 100
+  ebs_type = "gp2"
+
+  # On-demand to end the spot reclamations. c7i-flex.xlarge (4 vCPU / 8 GB)
+  # matches the prior c5a.xlarge footprint and clears the 4 GB JVM heap.
+  purchasing_option       = "on-demand"
+  on_demand_instance_type = "c7i-flex.xlarge"
+
+  # Distinct LT name so the module's launch template can coexist with the
+  # CFN-created master template during the cutover (no name collision,
+  # order-independent of delete-stack). The instance references the LT by ID.
+  launch_template_name = "CLOUDMasterTemplateTF"
+
+  # EKS-fronted for HTTP: DNS is external-dns -> ALB -> private IP over
+  # peering, admin via paws/SSM. create_eip stays true through the cutover
+  # (SSH path once the hostname resolves to the ALB); flipping to the
+  # ps80-style EIP-less shape is a follow-up.
+  create_eip            = true
+  create_route53_record = false
+
+  extra_master_managed_policies = [
+    "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy",
+  ]
+
+  extra_master_inline_policies = [
+    {
+      name = "AlloyGatewayBearerRead"
+      json = data.aws_iam_policy_document.cloud_alloy_bearer_read.json
+    },
+  ]
+
+  # :8080 from the EKS VPC over cross-region peering for the jenkins-ingress
+  # nginx (TLS offloaded at the ALB).
+  extra_http_ingress = [
+    { port = 8080, cidr = module.vpc.vpc_cidr_block },
+  ]
+
+  ssh_key_engineers = [
+    "anderson.nogueira",
+    "alex.miroshnychenko",
+    "eduardo.casarero",
+    "evgeniy.patlan",
+    "santiago.ruiz",
+    "surabhi.bhat",
+    "talha.rizwan",
+    "vadim.yalovets",
+  ]
+
+  # Declarative init.groovy.d delivered via the module-created S3 bucket
+  # (jenkins-cloud-init-config). Content moved byte-identically off the live
+  # master's EBS copy; cloud.groovy's netMap subnet IDs are patched to the
+  # module-created subnets right after the first apply (the IDs do not exist
+  # before it). `jenkins iac deploy` stays the no-restart hot-reload path.
+  init_groovy_files = {
+    for f in fileset("${path.module}/../resources/jenkins-masters/cloud/init.groovy.d", "*.groovy") :
+    f => file("${path.module}/../resources/jenkins-masters/cloud/init.groovy.d/${f}")
   }
 }
 
+# One-shot adopt of the retained CFN data volume (JENKINS_HOME). Remove this
+# block after the cutover apply lands.
+import {
+  to = module.cloud.aws_ebs_volume.data
+  id = "vol-077265221bc0180b3"
+}
+
+# ARM Graviton spot fleet for the ec2-fleet plugin -- the docker-aarch64
+# fallback. Pre-provisioned
+# Fleet-only while cloud was CFN-managed; now consumes the TF master's
+# VPC/subnets/role outputs.
 module "cloud_arm_fleet" {
   source    = "./modules/jenkins-arm-fleet"
   providers = { aws = aws.eu-west-1 }
 
   short_name                   = "jenkins-cloud"
   team                         = "cloud"
-  vpc_id                       = data.aws_vpc.cloud.id
-  subnet_ids                   = data.aws_subnets.cloud.ids
-  worker_instance_profile_name = "jenkins-cloud-worker"
-  master_role_name             = "jenkins-cloud-master"
+  vpc_id                       = module.cloud.vpc_id
+  subnet_ids                   = module.cloud.subnet_ids
+  worker_instance_profile_name = module.cloud.worker_instance_profile_name
+  master_role_name             = module.cloud.master_iam_role_name
   key_name                     = "percona-jenkins"
   instance_types               = ["m8g.2xlarge", "m7g.2xlarge", "m6g.2xlarge", "m7gd.2xlarge", "m6gd.2xlarge", "r8g.2xlarge", "r7g.2xlarge", "r6g.2xlarge"]
   max_size                     = 16
   tickets                      = "PS-11179"
+}
+
+# Rendered in the root so the ARN uses this account's caller-identity, not
+# the eu-west-1 alias's. Secret lives in us-east-1 with alloy-gateway.
+data "aws_iam_policy_document" "cloud_alloy_bearer_read" {
+  statement {
+    sid       = "AlloyGatewayBearerRead"
+    effect    = "Allow"
+    actions   = ["secretsmanager:GetSecretValue"]
+    resources = ["arn:aws:secretsmanager:us-east-1:${data.aws_caller_identity.current.account_id}:secret:percona-ci-platform/alloy-gateway/bearer-*"]
+  }
+}
+
+# Cross-region VPC peering EKS (us-east-1) <-> cloud (eu-west-1), so the
+# in-cluster jenkins-ingress nginx can reach the master's private IP on
+# :8080 (TLS offloaded at the jenkins-masters ALB). CIDRs are distinct: EKS
+# 10.220.0.0/16, cloud 10.177.0.0/22, ps3 10.181.0.0/22 + rel 10.199.0.0/22 (same region).
+
+resource "aws_vpc_peering_connection" "cloud" {
+  vpc_id      = module.vpc.vpc_id
+  peer_vpc_id = module.cloud.vpc_id
+  peer_region = "eu-west-1"
+  auto_accept = false
+
+  tags = {
+    Name = "${local.cluster_name}-to-jenkins-cloud"
+  }
+}
+
+resource "aws_vpc_peering_connection_accepter" "cloud" {
+  provider                  = aws.eu-west-1
+  vpc_peering_connection_id = aws_vpc_peering_connection.cloud.id
+  auto_accept               = true
+
+  tags = {
+    Name = "${local.cluster_name}-to-jenkins-cloud"
+  }
+}
+
+resource "aws_route" "eks_private_to_cloud" {
+  for_each                  = toset(module.vpc.private_route_table_ids)
+  route_table_id            = each.value
+  destination_cidr_block    = module.cloud.vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.cloud.id
+}
+
+# Statically-keyed map (not toset over the IDs): the cloud VPC is created in
+# the same apply, so the route-table IDs are unknown at plan time and a
+# value-derived set fails the plan. The module exposes exactly one private
+# route table.
+resource "aws_route" "cloud_to_eks" {
+  provider                  = aws.eu-west-1
+  for_each                  = { main = one(module.cloud.private_route_table_ids) }
+  route_table_id            = each.value
+  destination_cidr_block    = module.vpc.vpc_cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.cloud.id
 }
