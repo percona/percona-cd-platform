75dc820a216dddca36ba0874f8b91caec23b734c
Anderson Nogueira <anderson.nogueira@percona.com>
2026-09-14 16:10:12 +0000

    feat(ssh): enable the engineer roster sync on pxc, ps57 and pg
    - pmm, psmdb and pxb converged with all twelve checks passing

--- terraform/master-ps57.tf
+++ terraform/master-ps57.tf
@@ -25,6 +25,7 @@ module "ps57" {
   ami_id                  = nonsensitive(data.aws_ssm_parameter.al2023_minimal_euc1.value) # latest AL2023 minimal (amis.tf)
   master_profile          = "eks_observability"
   ssh_allowed_cidrs       = local.master_ssh_allowed_cidrs
+  engineer_roster         = local.master_ssh_engineer_roster
   jenkins_package_version = "2.568.3"
   java_package            = "java-21-amazon-corretto-headless"
   cache_bucket_name       = "ps-build-cache"
